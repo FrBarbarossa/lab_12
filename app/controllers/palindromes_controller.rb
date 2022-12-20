8683b64e18d74634adee14a6c49bf24156da3623
FrBarbarossa <makushinsp@mail.ru>
2022-12-20 11:34:31 +0300

    Codstyle fix

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -1,24 +1,25 @@
+# frozen_string_literal: true
+
+# Class for calculations
 class PalindromesController < ApplicationController
-    # before_action :check_num, only: :result
-    before_action :check_auth, only: :result
-  
-    def index
-    end
-  
-    def result
-      @new_result = Result.find_by(input: params[:number])
-      return unless @new_result.nil?
-      @new_result = Result.new(input: params[:number])
-      if @new_result.valid?
-        @new_result.save
-      else
-        redirect_to '/', alert: @new_result.errors['expiration_date'][0]
-      end
-    
-    end
+  # before_action :check_num, only: :result
+  before_action :check_auth, only: :result
 
-    def show
-      render xml: Result.all.to_xml
-    end
+  def index; end
 
+  def result
+    @new_result = Result.find_by(input: params[:number])
+    return unless @new_result.nil?
+
+    @new_result = Result.new(input: params[:number])
+    if @new_result.valid?
+      @new_result.save
+    else
+      redirect_to '/', alert: @new_result.errors['expiration_date'][0]
+    end
   end
+
+  def show
+    render xml: Result.all.to_xml
+  end
+end
